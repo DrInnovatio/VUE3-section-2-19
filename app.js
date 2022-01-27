ffb93124f3083823c8ef2d09ace7154fcda6e987
DrInnovatio <turing1723@gmail.com>
2022-01-27 18:41:30 +0900

    List Rendering.

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 let vm = Vue.createApp({
     data() {
         return {
-            mode: 1
+            birds: ['Pigeons', 'Eagles', 'Doves', 'Parrots'],
+            people: [
+                { name: 'John', age: 20 },
+                { name: 'Ricky', age: 18 },
+                { name: 'amy', age: 31 }
+            ]
         }
     }
 }).mount('#app');
